0a89d182b274906187ad2e47ac07f0c0855f988b
unknown <jacobdupea@gmail.com>
2023-09-01 23:36:29 -0700

    changed name to noteRoute and saved notes appear on /notes

--- Develop/routes/notesRoute.js
+++ Develop/routes/notesRoute.js
@@ -3,27 +3,26 @@ const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
 const uuid = require("../helpers/uuid");
 
 notes.get('/', (req, res) => {
-  readFromFile('./config/database.json').then((data) => res.json(JSON.parse(data)));
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 notes.post('/', (req, res) => {
   console.log(req.body);
 
-  const { title, note } = req.body;
+  const { title, text} = req.body;
 
   if (req.body) {
     const newNote = {
       title,
-      note,
+      text,
       note_id: uuid(),
     };
 
-    readAndAppend(newNote, './db/tips.json');
+    readAndAppend(newNote, './db/db.json');
     res.json(`Note added successfully`);
   } else {
     res.errored('Error in adding note')
   }
 });
-
 
 module.exports = notes;
--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const { clog } = require("./middleware/clog");
-const api = require('./routes/index');
+const api = require("./routes/index");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,8 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", api);
 
-const notesRouter = require("./routes/notes");
-app.use("/notes", notesRouter);
+const notesRouter = require("./routes/notesRoute");
+app.use("/notesRoute", notesRouter);
 
 app.use(express.static("public"));
 
